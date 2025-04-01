a7427e884e2164250c24b8ea47b0e967aa6129e7
Avichai98 <127767873+Avichai98@users.noreply.github.com>
2025-04-01 14:39:39 +0300

    Adding entities

---
+++ backend/services/kotlin-services/DataService/src/main/kotlin/app/dataservice/entities/VehicleEntity.kt
@@ -0,0 +1,38 @@
+package app.dataservice.entities
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.awt.Image
+import java.util.*
+
+@Document (collection = "vehicles")
+class VehicleEntity (
+    @Id var id: String?,
+    var type: String?,
+    var model: String?,
+    var manufacturer: String?,
+    var color: String?,
+    var imageUrl: String?,
+    var description: String?,
+    var timestamp: Date?,
+    var latitude: Double?,
+    var longitude: Double?
+) {
+    constructor(): this(null, null, null,null, null, null, null, null, null, null)
+
+    override fun toString(): String {
+        return "VehicleEntity(" +
+                "id=$id," +
+                " type=$type," +
+                " model=$model," +
+                " manufacturer=$manufacturer," +
+                " color=$color," +
+                " imageUrl=$imageUrl," +
+                " description=$description," +
+                " timestamp=$timestamp," +
+                " latitude=$latitude," +
+                " longitude=$longitude)"
+    }
+
+
+}
