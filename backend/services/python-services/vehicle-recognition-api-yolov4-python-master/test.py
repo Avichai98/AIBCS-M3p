ab01545fc68bfba4bebf93d0cd88974b6b981afe
Tomer Harel <tomerh13@gmail.com>
2025-05-06 08:30:42 +0300

    Refactor object detection logic into model class and update method for improved structure

--- backend/services/python-services/vehicle-recognition-api-yolov4-python-master/test.py
+++ backend/services/python-services/vehicle-recognition-api-yolov4-python-master/test.py
@@ -10,35 +10,41 @@ image = r'C:\Users\Tomer\Documents\visual studio code\AIBCS-M3p\backend\services
 lebels_colors = r'C:\Users\Tomer\Documents\visual studio code\AIBCS-M3p\backend\services\python-services\vehicle-recognition-api-yolov4-python-master\labels-colors.txt'
 lebels_makes = r'C:\Users\Tomer\Documents\visual studio code\AIBCS-M3p\backend\services\python-services\vehicle-recognition-api-yolov4-python-master\labels-makes.txt'
 coco_names = r'C:\Users\Tomer\Documents\visual studio code\AIBCS-M3p\backend\services\python-services\vehicle-recognition-api-yolov4-python-master\yolov4\coco.names'
-def objectDetect(image_path):
-    objects = []
-    LABELS = open(coco_names).read().strip().split("\n")
-    img = cv2.imread(image_path)
-    if img is None:
-        raise ValueError("Bad image")
+class model():
+    def __init__(self, yolocfg, yoloweights,modelbrandweights, modelcolorweights, labels_colors, labels_makes,coco_names):
+        self.net = cv2.dnn_DetectionModel(yolocfg, yoloweights)
+        self.net.setInputSize(608, 608)
+        self.net.setInputScale(1.0 / 255)
+        self.net.setInputSwapRB(True)
+        self.car_make_classifier = classifier.Classifier(modelbrandweights, labels_makes)
+        self.car_color_classifier = classifier.Classifier(modelcolorweights, labels_colors)
+        self.coco_names = coco_names
 
-    classes, confidences, boxes = net.detect(img, confThreshold=0.1, nmsThreshold=0.4)
-    for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
-        if classId in [2, 5, 7]:
-            if confidence > 0.3:
-                left, top, width, height = box
-                x1 = left
-                x2 = left + width
-                y1 = top
-                y2 = top + height
-                car_img = img[y1:y2, x1:x2]
-                (make, make_confidence) = car_make_classifier.predict(car_img)
-                (color, color_confidence) = car_color_classifier.predict(car_img)
-                rect = {"left": str(x1), "top": str(y1), "width": str(x2-x1), "height": str(y2-y1)}
-                objects.append({"object":LABELS[classId],"make": make, "color": color, "make_prob": str(make_confidence), "color_prob": str(color_confidence), "object_prob": str(confidence), "rect": rect})
+    def objectDetect(self,image_path):
+        objects = []
+        LABELS = open(coco_names).read().strip().split("\n")
+        img = cv2.imread(image_path)
+        if img is None:
+            raise ValueError("Bad image")
+
+        classes, confidences, boxes = self.net.detect(img, confThreshold=0.1, nmsThreshold=0.4)
+        for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
+            if classId in [2, 5, 7]:
+                if confidence > 0.3:
+                    left, top, width, height = box
+                    x1 = left
+                    x2 = left + width
+                    y1 = top
+                    y2 = top + height
+                    car_img = img[y1:y2, x1:x2]
+                    (make, make_confidence) = self.car_make_classifier.predict(car_img)
+                    (color, color_confidence) = self.car_color_classifier.predict(car_img)
+                    rect = {"left": str(x1), "top": str(y1), "width": str(x2-x1), "height": str(y2-y1)}
+                    objects.append({"object":LABELS[classId],"make": make, "color": color, "make_prob": str(make_confidence), "color_prob": str(color_confidence), "object_prob": str(confidence), "rect": rect})
+        
+        return {'vehicles': objects}
     
-    return {'vehicles': objects}
-    
-net = cv2.dnn_DetectionModel(yolocfg, yoloweights)
-net.setInputSize(608, 608)
-net.setInputScale(1.0 / 255)
-net.setInputSwapRB(True)
-car_make_classifier = classifier.Classifier(modelbrandweights, lebels_makes)
-car_color_classifier = classifier.Classifier(modelcolorweights, lebels_colors)
-output = objectDetect(image)
+
+mymodel = model(yolocfg, yoloweights,modelbrandweights, modelcolorweights, lebels_colors, lebels_makes,coco_names)
+output = mymodel.objectDetect(image)
 pprint.pprint(output)
