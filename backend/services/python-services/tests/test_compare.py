9547434b5a19aee2f2a2ed7f87b8773be2defba5
Avichai Shchori <127767873+Avichai98@users.noreply.github.com>
2025-07-19 19:39:52 +0300

    Update test_compare.py
    Adding a token in the test

--- backend/services/python-services/tests/test_compare.py
+++ backend/services/python-services/tests/test_compare.py
@@ -9,13 +9,16 @@ API_GET_URL = "http://localhost:8080/vehicles/getVehicles"
 
 @pytest.mark.asyncio
 async def test_vehicle_db_count_flow():
+    token = await get_auth_token()
+    headers = {"Authorization": f"Bearer {token}"}
+    
     async with httpx.AsyncClient() as client:
         # Step 1: clean up the database
-        response = await client.delete(API_DELETE_URL, timeout=60.0)
+        response = await client.delete(API_DELETE_URL, headers=headers, timeout=60.0)
         assert response.status_code == 200
 
         # Step 2: build models
-        response = await client.get(API_BUILD_URL, timeout=60.0)
+        response = await client.get(API_BUILD_URL, headers=headers, timeout=60.0)
         assert response.status_code == 200
         data = response.json()
         assert data["message"] == "Models built successfully"
@@ -23,13 +26,13 @@ async def test_vehicle_db_count_flow():
         time.sleep(2)
         # Step 3: Send first image
         with open("services/python-services/tests/test1.jpg", "rb") as f:
-            response = await client.post(API_URL, files={"file1": ("test1.jpg", f, "image/jpeg")}, timeout=60.0)
+            response = await client.post(API_URL, headers=headers, files={"file1": ("test1.jpg", f, "image/jpeg")}, timeout=60.0)
             assert response.status_code == 200
 
         time.sleep(3)
 
         # Check that there are 3 vehicles in the database
-        response = await client.get(API_GET_URL)
+        response = await client.get(API_GET_URL, headers=headers)
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, list)
@@ -39,13 +42,13 @@ async def test_vehicle_db_count_flow():
 
         # Step 4: Send second image
         with open("services/python-services/tests/test2.jpg", "rb") as f:
-            response = await client.post(API_URL, files={"file1": ("test2.jpg", f, "image/jpeg")}, timeout=60.0)
+            response = await client.post(API_URL, headers=headers, files={"file1": ("test2.jpg", f, "image/jpeg")}, timeout=60.0)
             assert response.status_code == 200
 
         time.sleep(3)
 
         # Check that there are now 5 vehicles in the database
-        response = await client.get(API_GET_URL)
+        response = await client.get(API_GET_URL, headers=headers)
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, list)
